f27c999ec437c5b90a9b1b34d654120b5e1f2abf
Karen Zilles <karen@zilles.com>
2020-09-16 09:09:52 -0700

    Don't need uses

--- public/protected/controllers/GraphCatController.php
+++ public/protected/controllers/GraphCatController.php
@@ -1,8 +1,4 @@
 <?php
-use League\CommonMark\CommonMarkConverter;
-use League\CommonMark\Environment;
-use League\CommonMark\Extension\Table\TableExtension;
-use League\CommonMark\Block\Element\Paragraph;
 
 class GraphCatController extends Controller
 {
